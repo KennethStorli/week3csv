c01b5d0c55549c9b3b9b31894782f3de245e1e9a
vavvar <haavard.kri@gmail.com>
2018-09-28 18:03:55 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
+    width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
   
@@ -12,6 +12,13 @@ var y = d3.scale.linear()
 
 var color = d3.scale.category10();
 
+var axisNames = { 
+                    petalWidth: 'Petal Width', 
+                    petalLength: 'Petal Length', 
+                    sepalWidth: 'Sepal Width', 
+                    sepalLength: 'Sepal Length' 
+                };
+
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
@@ -21,115 +28,123 @@ var yAxis = d3.svg.axis()
     .orient("left");
 
 
-var svg = d3.select("body").insert("svg",":first-child")
+var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-var xg = svg.append("g")
+
+
+d3.csv(urlcsv, function(error, data) {
+  data.forEach(function(d) {
+    d.petalLength = +d.petal_length;
+    d.petalWidth = +d.petal_width;
+    d.sepalLength = +d.sepal_length;
+    d.sepalWidth = +d.sepal_width;
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.petalWidth; })).nice();
+  y.domain(d3.extent(data, function(d) { return d.petalLength; })).nice();
+
+  svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")");
-xg
-  .append("text")
-  .attr("class", "label")
-  .attr("x", width)
-  .attr("y", -6)
-  .style("text-anchor", "end");
-      
-var yg = svg.append("g")
-      .attr("class", "y axis");
-yg
-  .append("text")
-  .attr("class", "label")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 6)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end");
-      
-var legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Petal Width (cm)");
 
-legend.append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Petal Length (cm)")
 
-legend.append("text")
-    .attr("x", width - 24)
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text(function(d) { return d; });
+ var circles = svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.petalWidth); })
+      .attr("cy", function(d) { return y(d.petalLength); })
+      .style("fill", function(d) { return color(d.species); });
 
-function update(){
-  
-  var xVar = d3.select('#xAXs').node().value,
-    yVar = d3.select('#yAXs').node().value;
-    
-  var checks = {};
-  d3.selectAll('input[type=checkbox]').each(function(){
-    checks[this.value] = this.checked;
+
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+
+
+  d3.selectAll("[name=v]").on("change", function() {
+      var selected = this.value;
+      display = this.checked ? "inline" : "none";
+
+
+  svg.selectAll(".dot")
+      .filter(function(d) {return selected == d.species;})
+      .attr("display", display);
+      });
+
+
+
+  d3.selectAll("[name=sepal]").on("change", function(d) {
+     radius = this.value;
+
+     svg.selectAll(".dot")
+     console.log(radius);
+     circles.attr("r", function(d) { return d[radius]; });
   });
-  
-  var radAttr = d3.select('input[type=radio]:checked').node().value;
-  
-  var data = baseData.filter(function(d,i){
-    d.x = d[xVar];
-    d.y = d[yVar];
-    return checks[d.species];
+
+
+
+  d3.select("[name=xAX]").on("change", function(){
+    xAxy = this.value;
+    console.log(xAxy)
+    x.domain(d3.extent(data, function(d) { return d[xAxy]; })).nice();
+
+    svg.select(".x.axis").transition().call(xAxis);
+
+    svg.selectAll(".dot").transition().attr("cx", function(d) { 
+        return x(d[xAxy]);
+    });
+    svg.selectAll(".x.axis").selectAll("text.label").text(axisNames[xAxy] + " (cm)");
   });
 
-  // set domains
-  x.domain(d3.extent(data, function(d) { return d.x; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.y; })).nice();
-
-  xg.call(xAxis);
-  yg.call(yAxis);
-  
-  xg.select("text").text(xVar);
-  yg.select("text").text(yVar);
-  
-  // on enter
-  var circles = svg.selectAll(".dot")
-    .data(data);
-    
-  circles.enter()
-    .append("circle")
-    .attr("class", "dot");
-    
-  circles.exit().remove();
-
-  // on update
-  circles.attr("cx", function(d) { return x(d.x); })
-    .attr("cy", function(d) { return y(d.y); })
-    .style("fill", function(d) { return color(d.species); })
-    .attr("r", function(d){ return d[radAttr]; });
-
-}
-
-var baseData = null;
-d3.csv("./iris.csv", function(error, csv) {
-  csv.forEach(function(d) {
-    d.petalLength = +d.petalLength;
-    d.petalWidth = +d.petalWidth;
-    d.sepalLength = +d.sepalLength;
-    d.sepalWidth = +d.sepalWidth;
+  d3.select("[name=yAX]").on("change", function(){
+    yAxy = this.value;
+    console.log(yAxy)
+    y.domain(d3.extent(data, function(d) { return d[yAxy]; })).nice();
+    svg.select(".y.axis").transition().call(yAxis);
+    svg.selectAll(".dot").transition().attr("cy", function(d) { 
+        return y(d[yAxy]);
+    });
+    svg.selectAll(".y.axis").selectAll("text.label").text(axisNames[yAxy] + " (cm)");
   });
-  baseData = csv;
-  update("petalWidth","petalLength");
+
 });
-
-d3.selectAll('select').on('change',function(){
-  update();
-});
-
-d3.selectAll('input').on('click', function(){
-  update();
-})
-
-
